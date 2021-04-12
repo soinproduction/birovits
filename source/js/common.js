d120376ce0736f863c88a21c1d7ef14302a513b2
Andrey <soinproduction@gmail.com>
2021-04-12 05:59:56 +0300

    add new pages

--- source/js/common.js
+++ source/js/common.js
@@ -1,5 +1,4 @@
-$likeButton = $(".like-btn");
-const accordionElement = document.getElementsByClassName("accordion");
+// -----------------  Слайдера --------------------
 
 const Sliders = {
   INFORM: {
@@ -34,10 +33,13 @@ const Sliders = {
   NAVBAR: {
     ELEMENT: $('.navbar-slider'),
     SETTINGS: {
-
+      variableWidth: true,
+      dots: true,
+      speed: 500,
+      dotsClass: "baner-slider__dots",
     },
-    BREAKPOINT: 500,
-    CLASSNAME: '',
+    BREAKPOINT: 768,
+    CLASSNAME: 'navbar-slider--flex',
   },
   FIRST_SLIDER: {
     ELEMENT: $('.baner-slider'),
@@ -52,105 +54,122 @@ const Sliders = {
     // BREAKPOINT: OFF_SLIDER,
     CLASSNAME: '',
   },
+  BRAND_SLIDER: {
+    ELEMENT: $('.brand-slider'),
+    SETTINGS: {
+      accessibility: true,
+      arrows: true,
+      dots: true,
+      speed: 500,
+      slidesToShow: 6,
+      prevArrow: '<div class="novelty-slider-left novelty-slider__button"><img src="img/slider-left.svg" alt="Слайд"></div>',
+      nextArrow: '<div class="novelty-slider-right novelty-slider__button"><img src="img/slider-right.svg" alt="Слайд"></div>',
+      dotsClass: "baner-slider__dots",
+    },
+    // BREAKPOINT: OFF_SLIDER,
+    CLASSNAME: '',
+  },
+  HOT_SLIDER: {
+    ELEMENT: $('.hot-slider'),
+    SETTINGS: {
+      accessibility: true,
+      arrows: true,
+      dots: true,
+      speed: 1500,
+      slidesToShow: 3,
+      arrows: false,
+      prevArrow: '<div class="novelty-slider-left novelty-slider__button"><img src="img/slider-left.svg" alt="Слайд"></div>',
+      nextArrow: '<div class="novelty-slider-right novelty-slider__button"><img src="img/slider-right.svg" alt="Слайд"></div>',
+      dotsClass: "baner-slider__dots",
+    },
+    // BREAKPOINT: OFF_SLIDER,
+    CLASSNAME: '',
+  },
+  LIDERS_SLIDER: {
+    ELEMENT: $('.liders-slider'),
+    SETTINGS: {
+      accessibility: true,
+      arrows: true,
+      dots: true,
+      speed: 1500,
+      rows: 2,
+      slidesToShow: 3,
+      arrows: false,
+      prevArrow: '<div class="novelty-slider-left novelty-slider__button"><img src="img/slider-left.svg" alt="Слайд"></div>',
+      nextArrow: '<div class="novelty-slider-right novelty-slider__button"><img src="img/slider-right.svg" alt="Слайд"></div>',
+      dotsClass: "baner-slider__dots",
+    },
+    // BREAKPOINT: OFF_SLIDER,
+    CLASSNAME: '',
+  },
+  NOVELTY_SLIDER: {
+    ELEMENT: $('.novelty-slider'),
+    SETTINGS: {
+      accessibility: true,
+      arrows: true,
+      dots: false,
+      speed: 1500,
+      slidesToShow: 5,
+      arrows: true,
+      prevArrow: '<div class="novelty-slider-left novelty-slider__button"><img src="img/slider-left.svg" alt="Слайд"></div>',
+      nextArrow: '<div class="novelty-slider-right novelty-slider__button"><img src="img/slider-right.svg" alt="Слайд"></div>',
+      dotsClass: "baner-slider__dots",
+    },
+    // BREAKPOINT: OFF_SLIDER,
+    CLASSNAME: '',
+  },
 }
 
 
 function initialazeSlickSlider(slider) {
-  const {BREAKPOINT, SETTINGS, ELEMENT} = slider;
+  const {
+    BREAKPOINT,
+    SETTINGS,
+    ELEMENT
+  } = slider;
   (document.documentElement.clientWidth <= BREAKPOINT || BREAKPOINT === undefined) && ELEMENT.slick(SETTINGS);
 }
 
 function toggleSlider(slider) {
-  const {BREAKPOINT, ELEMENT, SETTINGS} = slider;
+  const {
+    BREAKPOINT,
+    ELEMENT,
+    SETTINGS
+  } = slider;
   document.documentElement.clientWidth > BREAKPOINT && ELEMENT.hasClass('slick-initialized') && ELEMENT.slick('unslick');
   document.documentElement.clientWidth <= BREAKPOINT && !ELEMENT.hasClass('slick-initialized') && ELEMENT.slick(SETTINGS);
 }
 
 function toggleExtraClass(slider) {
-  const {BREAKPOINT, ELEMENT, CLASSNAME} = slider;
+  const {
+    BREAKPOINT,
+    ELEMENT,
+    CLASSNAME
+  } = slider;
   document.documentElement.clientWidth > BREAKPOINT && !ELEMENT.hasClass(CLASSNAME) && ELEMENT.addClass(CLASSNAME);
   document.documentElement.clientWidth <= BREAKPOINT && ELEMENT.hasClass(CLASSNAME) && ELEMENT.removeClass(CLASSNAME);
 }
 
 initialazeSlickSlider(Sliders.INFORM);
+initialazeSlickSlider(Sliders.NAVBAR);
 initialazeSlickSlider(Sliders.FIRST_SLIDER);
+initialazeSlickSlider(Sliders.BRAND_SLIDER);
+initialazeSlickSlider(Sliders.HOT_SLIDER);
+initialazeSlickSlider(Sliders.LIDERS_SLIDER);
+initialazeSlickSlider(Sliders.NOVELTY_SLIDER);
 
 window.addEventListener('resize', () => {
   toggleSlider(Sliders.INFORM);
   toggleExtraClass(Sliders.INFORM);
 });
 
-
-
-
-
-$(".novelty-slider").slick({
-  accessibility: true,
-  arrows: true,
-  dots: false,
-  speed: 1500,
-  slidesToShow: 5,
-  arrows: true,
-  prevArrow: '<div class="novelty-slider-left novelty-slider__button"><img src="img/slider-left.svg" alt="Слайд"></div>',
-  nextArrow: '<div class="novelty-slider-right novelty-slider__button"><img src="img/slider-right.svg" alt="Слайд"></div>',
-  dotsClass: "baner-slider__dots",
-});
-
-$(".liders-slider").slick({
-  accessibility: true,
-  arrows: true,
-  dots: true,
-  speed: 1500,
-  rows: 2,
-  slidesToShow: 3,
-  arrows: false,
-  prevArrow: '<div class="novelty-slider-left novelty-slider__button"><img src="img/slider-left.svg" alt="Слайд"></div>',
-  nextArrow: '<div class="novelty-slider-right novelty-slider__button"><img src="img/slider-right.svg" alt="Слайд"></div>',
-  dotsClass: "baner-slider__dots",
-});
-
-$(".hot-slider").slick({
-  accessibility: true,
-  arrows: true,
-  dots: true,
-  speed: 1500,
-  slidesToShow: 3,
-  arrows: false,
-  prevArrow: '<div class="novelty-slider-left novelty-slider__button"><img src="img/slider-left.svg" alt="Слайд"></div>',
-  nextArrow: '<div class="novelty-slider-right novelty-slider__button"><img src="img/slider-right.svg" alt="Слайд"></div>',
-  dotsClass: "baner-slider__dots",
-});
-
-$(".brand-slider").slick({
-  accessibility: true,
-  arrows: true,
-  dots: true,
-  speed: 500,
-  slidesToShow: 6,
-  prevArrow: '<div class="novelty-slider-left novelty-slider__button"><img src="img/slider-left.svg" alt="Слайд"></div>',
-  nextArrow: '<div class="novelty-slider-right novelty-slider__button"><img src="img/slider-right.svg" alt="Слайд"></div>',
-  dotsClass: "baner-slider__dots",
-});
-
-
+// -----------------  Лайк  --------------------
+$likeButton = $(".like-btn");
 $likeButton.click(function () {
   $(this).toggleClass("like-btn--active");
 });
 
-
-
-for (var i = 0; i < accordionElement.length; i++) {
-  accordionElement[i].addEventListener("click", function () {
-    this.classList.toggle("accordion__active");
-    var panel = this.nextElementSibling;
-    if (panel.style.maxHeight) {
-      panel.style.maxHeight = null;
-    } else {
-      panel.style.maxHeight = panel.scrollHeight + "px";
-    }
-  });
-}
-
+// -----------------  Табы  --------------------
 $(".tab").on("click", function (e) {
   e.preventDefault();
   $(".tab-section").removeClass("tabs-content--active");
@@ -159,6 +178,8 @@ $(".tab").on("click", function (e) {
   $($(this).attr("href")).addClass("tabs-content--active");
   $(this).addClass("tab_active");
 });
+
+// -----------------  Селект  --------------------
 
 // переменная не переназначается, поэтому используем const
 // используем querySelectorAll, чтобы собрать массив со всеми сущностями .select
@@ -184,29 +205,152 @@ if (select.length) {
   });
 }
 
-$(".block-count__plus").click(function () {
-  var max_qty = Number(
-    $(this).parent().find(".block-count__input").attr("max")
-  );
-  var qty = Number($(this).parent().find(".block-count__input").val());
-  if (qty > max_qty) {
-    qty = max_qty;
-  } else if (qty == max_qty) {
-    qty = max_qty;
-  } else {
-    qty += 1;
-  }
-  $(this).parent().find(".block-count__input").val(qty);
+// ---------------- Счетчик ---------------------
+$(function () {
+  (function quantityProducts() {
+    var $quantityArrowMinus = $(".block-count__minus");
+    var $quantityArrowPlus = $(".block-count__plus");
+
+    $quantityArrowMinus.click(quantityMinus);
+    $quantityArrowPlus.click(quantityPlus);
+
+    function quantityMinus() {
+      var $quantityNum = $(this).siblings('.block-count__input');
+      if ($quantityNum.val() > 1) {
+        $quantityNum.val(+$quantityNum.val() - 1);
+      }
+    }
+
+    function quantityPlus() {
+      var $quantityNum = $(this).siblings('.block-count__input');
+      $quantityNum.val(+$quantityNum.val() + 1);
+    }
+  })();
 });
 
-$(".block-count__minus").click(function () {
-  if ($(this).parent().find(".block-count__input").val() > 1) {
-    var plus = Number($(this).parent().find(".block-count__input").val()) - 1;
-    $(this).parent().find(".block-count__input").val(plus);
-  }
-});
-
+// -----------------  Гамбургер  --------------------
 $(".hamburger").click(function (event) {
   $(".hamburger").toggleClass("hamburger__active"),
     $(".mobile__menu ").toggleClass("mobile__menu__active");
+});
+
+// ----------------- Аккордион --------------------
+/**
+ * Классы для аккордиона
+ */
+const accordeon = {
+  CLASS: 'accordion',
+  CLASS_ACTIVE: 'accordion__active',
+}
+
+/**
+ * acc - неизменная переменная для работы с аккордионом
+ */
+const acc = document.querySelectorAll(`.${accordeon.CLASS}`);
+let openedAccordeon = null;
+
+/**
+ * использует nextElementSibling для открытия или закрытия аккордиона
+ */
+function closeAccordeon(acc) {
+  acc.nextElementSibling.style.maxHeight = 0;
+  acc.classList.remove(accordeon.CLASS_ACTIVE);
+}
+
+
+function openAccordeon(acc) {
+  acc.nextElementSibling.style.maxHeight = `${acc.nextElementSibling.scrollHeight}px`;
+  acc.classList.add(accordeon.CLASS_ACTIVE);
+}
+
+/**
+ * Проверка на открытие аккордиона !nextElementSibling!
+ */
+function isAccordeonOpen(acc) {
+  acc.nextElementSibling && !acc.nextElementSibling.style.maxHeight
+}
+
+/**
+ * Итерация, реализация переключения открытого аккордиона
+ *
+ */
+for (const accordeon of acc) {
+  accordeon.addEventListener("click", function () {
+    const currentAccordeon = this;
+
+    openedAccordeon && closeAccordeon(openedAccordeon);
+
+    if (isAccordeonOpen(currentAccordeon)) {
+      closeAccordeon(currentAccordeon);
+    } else {
+      openAccordeon(currentAccordeon);
+      openedAccordeon = currentAccordeon;
+    }
+  });
+};
+
+// --------------
+
+const accordionList = document.getElementsByClassName("accordion--multiple");
+const classNameActive = "accordion--active";
+
+for (const accordion of accordionList) {
+  accordion.addEventListener("click", function () {
+        this.classList.toggle(classNameActive);
+        const panel = this.nextElementSibling;
+        if (panel.style.maxHeight) {
+            panel.style.maxHeight = null;
+        } else {
+            panel.style.maxHeight = panel.scrollHeight + "px";
+        }
+    });
+}
+
+// ------------- Ползунок --------------------
+$(".polzunok-5").slider({
+  min: 0,
+  max: 500,
+  values: [70, 200],
+  range: true,
+  animate: "fast",
+  slide : function(event, ui) {
+      $(".polzunok-input-5-left").val(ui.values[ 0 ]);
+      $(".polzunok-input-5-right").val(ui.values[ 1 ]);
+  }
+});
+$(".polzunok-input-5-left").val($(".polzunok-5").slider("values", 0));
+$(".polzunok-input-5-right").val($(".polzunok-5").slider("values", 1));
+$(".polzunok-container-5 input").change(function() {
+  var input_left = $(".polzunok-input-5-left").val().replace(/[^0-9]/g, ''),
+  opt_left = $(".polzunok-5").slider("option", "min"),
+  where_right = $(".polzunok-5").slider("values", 1),
+  input_right = $(".polzunok-input-5-right").val().replace(/[^0-9]/g, ''),
+  opt_right = $(".polzunok-5").slider("option", "max"),
+  where_left = $(".polzunok-5").slider("values", 0);
+  if (input_left > where_right) {
+      input_left = where_right;
+  }
+  if (input_left < opt_left) {
+      input_left = opt_left;
+  }
+  if (input_left == "") {
+  input_left = 0;
+  }
+  if (input_right < where_left) {
+      input_right = where_left;
+  }
+  if (input_right > opt_right) {
+      input_right = opt_right;
+  }
+  if (input_right == "") {
+  input_right = 0;
+  }
+  $(".polzunok-input-5-left").val(input_left);
+  $(".polzunok-input-5-right").val(input_right);
+  if (input_left != where_left) {
+      $(".polzunok-5").slider("values", 0, input_left);
+  }
+  if (input_right != where_right) {
+      $(".polzunok-5").slider("values", 1, input_right);
+  }
 });
