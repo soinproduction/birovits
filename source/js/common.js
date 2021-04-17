fd8c4888de577758c403619e9487acf76cd32a01
Andrey <soinproduction@gmail.com>
2021-04-17 06:53:08 +0300

    add new pages

--- source/js/common.js
+++ source/js/common.js
@@ -54,6 +54,40 @@ const Sliders = {
     // BREAKPOINT: OFF_SLIDER,
     CLASSNAME: '',
   },
+  PROD_SLIDER: {
+    ELEMENT: $('.product-slider'),
+    SETTINGS: {
+      accessibility: true,
+      arrows: false,
+      dots: false,
+      speed: 1500,
+      dotsClass: "baner-slider__dots",
+      slidesToShow: 1,
+      infinite: false,
+      asNavFor: '.product-nav',
+      swipe: false,
+    },
+    // BREAKPOINT: OFF_SLIDER,
+    CLASSNAME: '',
+  },
+  NAV_SLIDER: {
+    ELEMENT: $('.product-nav'),
+    SETTINGS: {
+      accessibility: true,
+      arrows: true,
+      dots: false,
+      speed: 1500,
+      dotsClass: "baner-slider__dots",
+      slidesToShow: 4,
+      slidesToScroll: 1,
+      infinite: false,
+      prevArrow: $('.product-nav-arrow-left'),
+      nextArrow: $('.product-nav-arrow-right'),
+      asNavFor: '.product-slider',
+    },
+    // BREAKPOINT: OFF_SLIDER,
+    CLASSNAME: '',
+  },
   BRAND_SLIDER: {
     ELEMENT: $('.brand-slider'),
     SETTINGS: {
@@ -157,6 +191,8 @@ initialazeSlickSlider(Sliders.BRAND_SLIDER);
 initialazeSlickSlider(Sliders.HOT_SLIDER);
 initialazeSlickSlider(Sliders.LIDERS_SLIDER);
 initialazeSlickSlider(Sliders.NOVELTY_SLIDER);
+initialazeSlickSlider(Sliders.PROD_SLIDER);
+initialazeSlickSlider(Sliders.NAV_SLIDER);
 
 window.addEventListener('resize', () => {
   toggleSlider(Sliders.INFORM);
@@ -291,14 +327,14 @@ const classNameActive = "accordion--active";
 
 for (const accordion of accordionList) {
   accordion.addEventListener("click", function () {
-        this.classList.toggle(classNameActive);
-        const panel = this.nextElementSibling;
-        if (panel.style.maxHeight) {
-            panel.style.maxHeight = null;
-        } else {
-            panel.style.maxHeight = panel.scrollHeight + "px";
-        }
-    });
+    this.classList.toggle(classNameActive);
+    const panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
 }
 
 // ------------- Ползунок --------------------
@@ -326,67 +362,87 @@ $(".polzunok-5").slider({
   values: [100, 300],
   range: true,
   animate: "fast",
-  slide : function(event, ui) {
-      $(".polzunok-input-5-left").val(ui.values[ 0 ]);
-      $(".polzunok-input-5-right").val(ui.values[ 1 ]);
+  slide: function (event, ui) {
+    $(".polzunok-input-5-left").val(ui.values[0]);
+    $(".polzunok-input-5-right").val(ui.values[1]);
   }
 });
 $(".polzunok-input-5-left").val($(".polzunok-5").slider("values", 0));
 $(".polzunok-input-5-right").val($(".polzunok-5").slider("values", 1));
-$(".polzunok-container-5 input").change(function() {
+$(".polzunok-container-5 input").change(function () {
   var input_left = $(".polzunok-input-5-left").val().replace(/[^0-9]/g, ''),
-  opt_left = $(".polzunok-5").slider("option", "min"),
-  where_right = $(".polzunok-5").slider("values", 1),
-  input_right = $(".polzunok-input-5-right").val().replace(/[^0-9]/g, ''),
-  opt_right = $(".polzunok-5").slider("option", "max"),
-  where_left = $(".polzunok-5").slider("values", 0);
+    opt_left = $(".polzunok-5").slider("option", "min"),
+    where_right = $(".polzunok-5").slider("values", 1),
+    input_right = $(".polzunok-input-5-right").val().replace(/[^0-9]/g, ''),
+    opt_right = $(".polzunok-5").slider("option", "max"),
+    where_left = $(".polzunok-5").slider("values", 0);
   if (input_left > where_right) {
-      input_left = where_right;
+    input_left = where_right;
   }
   if (input_left < opt_left) {
-      input_left = opt_left;
+    input_left = opt_left;
   }
   if (input_left == "") {
-  input_left = 0;
+    input_left = 0;
   }
   if (input_right < where_left) {
-      input_right = where_left;
+    input_right = where_left;
   }
   if (input_right > opt_right) {
-      input_right = opt_right;
+    input_right = opt_right;
   }
   if (input_right == "") {
-  input_right = 0;
+    input_right = 0;
   }
   $(".polzunok-input-5-left").val(input_left);
   $(".polzunok-input-5-right").val(input_right);
   if (input_left != where_left) {
-      $(".polzunok-5").slider("values", 0, input_left);
+    $(".polzunok-5").slider("values", 0, input_left);
   }
   if (input_right != where_right) {
-      $(".polzunok-5").slider("values", 1, input_right);
+    $(".polzunok-5").slider("values", 1, input_right);
   }
 });
 
 
-$('body').on('click', '.password-control', function(){
-	if ($('#password-input').attr('type') == 'password'){
-		$(this).addClass('view');
-		$('#password-input').attr('type', 'text');
-	} else {
-		$(this).removeClass('view');
-		$('#password-input').attr('type', 'password');
-	}
-	return false;
+$('body').on('click', '.password-control', function () {
+  if ($('#password-input').attr('type') == 'password') {
+    $(this).addClass('view');
+    $('#password-input').attr('type', 'text');
+  } else {
+    $(this).removeClass('view');
+    $('#password-input').attr('type', 'password');
+  }
+  return false;
 });
 
-$('body').on('click', '.password-control2', function(){
-	if ($('#password-input2').attr('type') == 'password'){
-		$(this).addClass('view');
-		$('#password-input2').attr('type', 'text');
-	} else {
-		$(this).removeClass('view');
-		$('#password-input2').attr('type', 'password');
-	}
-	return false;
+$('body').on('click', '.password-control2', function () {
+  if ($('#password-input2').attr('type') == 'password') {
+    $(this).addClass('view');
+    $('#password-input2').attr('type', 'text');
+  } else {
+    $(this).removeClass('view');
+    $('#password-input2').attr('type', 'password');
+  }
+  return false;
 });
+
+
+! function () {
+  var i, u, p, y, m, g, e = document.querySelectorAll(".yt-lazyload");
+  0 < e.length && (u = document.createElement("div"), p = document.createElement("div"), y = document.createElement("div"), m = document.createElement("a"), g = document.createElement("iframe"), u.classList.add("yt-lazyload-wrap"), p.classList.add("yt-lazyload-content"), y.classList.add("yt-lazyload-playbtn"), m.classList.add("yt-lazyload-logo"), m.target = "_blank", m.rel = "noreferrer", g.setAttribute("allow", "accelerometer;autoplay;encrypted-media;gyroscope;picture-in-picture"), g.setAttribute("allowfullscreen", ""), i = new IntersectionObserver(function (e) {
+    e.forEach(function (e) {
+      var t, a, o, n, r, l = e.target,
+        d = e.target.dataset.id,
+        c = e.target.dataset.thumb,
+        s = e.target.dataset.logo;
+      !0 === e.isIntersecting && (t = u.cloneNode(), l.append(t), a = p.cloneNode(), t.append(a), a.style.setProperty("--yt-lazyload-img", 'url("https://img.youtube.com/vi/' + d + c + '/maxresdefault.jpg")'), o = y.cloneNode(), a.append(o), "0" !== s && ((n = m.cloneNode()).href = "https://youtu.be/" + d, a.append(n)), o.addEventListener("click", function () {
+        (r = g.cloneNode()).src = "https://www.youtube.com/embed/" + d + "?autoplay=1", a.append(r)
+      }), i.unobserve(l))
+    })
+  }, {
+    rootMargin: "200px 0px"
+  }), e.forEach(function (e) {
+    i.observe(e)
+  }))
+}();
